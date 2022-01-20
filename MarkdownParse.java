0f2dc12c0f14147a9651126aad726aabd55d1fd4
AmaarValliani <avallian@ucsd.edu>
2022-01-19 18:53:16 -0800

    fixed image bug
    no longer finds images in markdown file

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -19,7 +19,7 @@ public class MarkdownParse {
             if(nextOpenBracket==-1 || nextCloseBracket==-1
                             || openParen==-1 || closeParen==-1)
                 break;
-            if(nextCloseBracket+1 == openParen)
+            if(nextCloseBracket+1 == openParen || !markdown.substring(nextOpenBracket-1, nextOpenBracket).equals("!"))
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
--- test-file.md
+++ test-file.md
@@ -2,3 +2,7 @@
 
 [a link!](https://something.com)
 [another link!](some-page.html)
+[a link!]fafa(https://something.com)
+![a link!](https://something.com)
+
+some other stuff
